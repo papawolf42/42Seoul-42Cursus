15703a19d2d4f22a35b41255c267184e50d47be3
김건우 <papawolf@kakao.com>
2021-01-12 17:45:26 +0900

    21.01.12 Make handle with comes with '-' and '+' flag

--- 01_ft_printf/printf/src/ft_print_integer2.c
+++ 01_ft_printf/printf/src/ft_print_integer2.c
@@ -6,7 +6,7 @@
 /*   By: gunkim <gunkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 01:00:41 by gunkim            #+#    #+#             */
-/*   Updated: 2021/01/12 15:13:09 by gunkim           ###   ########.fr       */
+/*   Updated: 2021/01/12 17:44:22 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ void		ft_itoa_custom(t_fmt *fmt, t_blk *blk)
 		blk->minus = 1;
 	}
 	blk->nbr = ft_count_digit(temp);
-	if (temp > 0 && fmt->flag[plus] > 0 && fmt->flag[space] == 0)
+	if (fmt->flag[minus] == 0 && fmt->flag[plus] > 0 && fmt->flag[space] == 0)
 		blk->plus = 1;
-	if (temp > 0 && fmt->flag[space] > 0 && fmt->flag[plus] == 0)
+	if (fmt->flag[space] > 0 && fmt->flag[plus] == 0)
 		blk->space = 1;
 	i = blk->nbr;
 	while (temp >= 1)
