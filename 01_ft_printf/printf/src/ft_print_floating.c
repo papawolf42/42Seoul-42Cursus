5273f526bc5bf0d1e005c9fd47feb1d632c37b21
papawolf42 <papawolf@kakao.com>
2021-02-15 00:34:45 +0900

    21.02.15 Fix denomalization

--- 01_ft_printf/printf/src/ft_print_floating.c
+++ 01_ft_printf/printf/src/ft_print_floating.c
@@ -6,7 +6,7 @@
 /*   By: gunkim <gunkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/22 22:27:24 by gunkim            #+#    #+#             */
-/*   Updated: 2021/02/07 12:07:55 by gunkim           ###   ########.fr       */
+/*   Updated: 2021/02/16 13:07:14 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,11 +42,11 @@ void			ft_get_52_bit(t_dbl *dbl, t_big *big)
 	int				i;
 	const size_t	mantissa = dbl->s_dbl.mtsa;
 
-	big->is_zero = mantissa == 0 && dbl->s_dbl.expo == 0 ? 1 : 0;
+	big->is_zero = dbl->s_dbl.expo == 0 ? 1 : 0;
 	big->mtsa[52] = big->is_zero ? '0' : '1';
 	i = 52;
-	while (i--)
-		big->mtsa[i] = (((mantissa >> i) & 1) > 0) + '0';
+	while (i-- + big->is_zero > 0)
+		big->mtsa[i + big->is_zero] = (((mantissa >> i) & 1) > 0) + '0';
 }
 
 int				ft_print_f(t_fmt *fmt, t_dbl *dbl, t_big *big, t_blk *blk)
