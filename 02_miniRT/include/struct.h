aaebadc65de4f51f6eb95b4998dd81cde6abcad4
papawolf42 <papawolf@kakao.com>
2021-04-21 23:51:23 +0900

    21.04.21 Declare and use structures

--- 02_miniRT/include/struct.h
+++ 02_miniRT/include/struct.h
@@ -6,27 +6,54 @@
 /*   By: gunkim <gunkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 14:54:12 by gunkim            #+#    #+#             */
-/*   Updated: 2021/04/08 19:07:11 by gunkim           ###   ########.fr       */
+/*   Updated: 2021/04/21 23:33:09 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCT_H
 # define STRUCT_H
 
-typedef struct	s_image
+typedef struct s_vec3		t_vec3;
+typedef struct s_matrix		t_matrix;
+typedef struct s_image		t_image;
+typedef struct s_scene		t_scene;
+typedef struct s_ctrl		t_ctrl;
+
+struct				s_vec3
+{
+	double		x;
+	double		y;
+	double		z;
+};
+
+struct				s_matrix
+{
+	t_vec3		vx;
+	t_vec3		vy;
+	t_vec3		vz;
+};
+
+struct				s_image
 {
 	void		*img_ptr;
 	char		*data;
 	int			bit_per_pixel;
 	int			size_line;
 	int			endian;
-}				t_image;
+};
 
-typedef struct	s_ctrl
+struct				s_scene
+{
+	int			width;
+	int			height;
+};
+
+struct				s_ctrl
 {
 	void		*mlx_ptr;
 	void		*win_ptr;
 	t_image		img;
-}				t_ctrl;
+	t_scene		scene;
+};
 
 #endif
--- 02_miniRT/src/parse/parse.c
+++ 02_miniRT/src/parse/parse.c
@@ -1,39 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minirt.h                                           :+:      :+:    :+:   */
+/*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gunkim <gunkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/07 14:47:25 by gunkim            #+#    #+#             */
-/*   Updated: 2021/04/21 23:43:52 by gunkim           ###   ########.fr       */
+/*   Created: 2021/04/20 17:31:51 by gunkim            #+#    #+#             */
+/*   Updated: 2021/04/21 23:45:22 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINIRT_H
-# define MINIRT_H
+#include "minirt.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include "mlx.h"
-# include "libft.h"
-# include "struct.h"
-
-/*
-** main.c
-*/
-void		ft_render(t_ctrl *ctrl);
-void		ft_minirt(t_ctrl *ctrl);
-int			main(int argc, char *argv[]);
-
-/*
-** parse.c
-*/
-void		ft_read_rt(t_ctrl *ctrl, char *rt_file);
-
-/*
-** utils_str.c
-*/
-int			ft_is_endstr(char *big, char *little);
-
-#endif
+void		ft_read_rt(t_ctrl *ctrl, char *rt_file)
+{
+	ctrl->scene.width = 1024;
+	ctrl->scene.height = 768;
+	
+	/* to avoid warning */
+	rt_file = NULL;
+	/* to avoid warning */
+}
